4a03cca8d505bc5b3e91af57c8878734bfe8332b
veerupaji <30898440+veerupaji@users.noreply.github.com>
2017-08-13 11:51:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'aticle-one.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 
